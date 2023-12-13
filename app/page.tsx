e14b0e7bc0511735db103920c52f40c4be40da09
crackcodesprogramloads <mikepadial@gmail.com>
2023-12-13 13:20:45 +0800

    Add wagmi read and write skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
-import Image from "next/image";
 import Card from "./components/Card";
+import { useContractWrite } from "wagmi";
 
 export default function Home() {
+  // const { data, isLoading, isSuccess, write } = useContractWrite({
+  //   address: "0xecb504d39723b0be0e3a9aa33d646642d1051ee1",
+  //   abi: wagmigotchiABI,
+  //   functionName: "feed",
+  // });
+
+  // const { data, isError, isLoading, read } = useContractRead({
+  //   address: "0xecb504d39723b0be0e3a9aa33d646642d1051ee1",
+  //   abi: wagmigotchiABI,
+  //   functionName: "getHunger",
+  // });
   return (
     <main className="pt-20 h-screen flex flex-col items-center justify-around">
       <div className="flex flex-row justify-center gap-10">
-        <button className="px-4 py-2 font-semibold rounded-xl bg-[#7b3fe4]">
+        <button
+          // onClick={() => write()}
+          className="px-4 py-2 font-semibold rounded-xl bg-[#7b3fe4]"
+        >
           Mint NFT
         </button>
-        <button className="px-4 py-2 font-semibold rounded-xl bg-[#7b3fe4]">
+        <button // onClick={() => read()}
+          className="px-4 py-2 font-semibold rounded-xl bg-[#7b3fe4]"
+        >
           Check your NFT&apos;s
         </button>
       </div>
